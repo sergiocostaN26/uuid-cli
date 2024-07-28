56a1c994d52f11a42f845e17f7b9da19e82be7c9
sergiocostamoreno <47816978+sergiocostamoreno@users.noreply.github.com>
2024-07-28 21:33:49 +0200

    Parse Instant if provided to the CLI

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,3 +1,23 @@
+import kotlin.system.exitProcess
+import kotlinx.datetime.Instant
+
 fun main(args: Array<String>) {
-    println(Generate.now())
+    if (args.isEmpty()) {
+        Generate.now()
+    } else {
+        args[0].asInstant().let(Generate::at)
+    }.let(::println)
+}
+
+private fun String.asInstant(): Instant {
+    val fromLong = runCatching { Instant.fromEpochMilliseconds(this.toLong()) }.getOrNull()
+    val fromOffsetTimestamp = runCatching { Instant.parse(this) }.getOrNull()
+    return when {
+        fromLong != null -> fromLong
+        fromOffsetTimestamp != null -> fromOffsetTimestamp
+        else -> {
+            println("The timestamp provided cannot be parsed")
+            exitProcess(1)
+        }
+    }
 }
