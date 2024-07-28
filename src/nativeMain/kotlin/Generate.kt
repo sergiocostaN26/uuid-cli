8846a5b6b21220b2e3df17b5ae3f24a027801220
sergiocostamoreno <47816978+sergiocostamoreno@users.noreply.github.com>
2024-07-28 20:58:51 +0200

    Code refactor to enable other use cases

---
+++ src/nativeMain/kotlin/Generate.kt
@@ -0,0 +1,27 @@
+import kotlin.random.Random
+import kotlinx.datetime.Clock
+
+private const val VERSION: Long = 7
+private const val VARIANT: Long = 0b10
+private const val RANDOM_A_TOP_LIMIT: Long = 0x1000 // 12 random bits
+private const val RANDOM_B_TOP_LIMIT: Long = 0x4000000000000000 // 62 random bits
+
+object Generate {
+
+    fun now(): String {
+        val timestamp = Clock.System.now().toEpochMilliseconds()
+        val randomA = Random.nextLong(RANDOM_A_TOP_LIMIT)
+        val randomB = Random.nextLong(RANDOM_B_TOP_LIMIT)
+        return setOf(
+            timestamp shr 16 to 8,
+            timestamp and 0xffff to 4,
+            (VERSION shl 12) + randomA to 4,
+            (VARIANT shl 14) + (randomB shr 48) to 4,
+            randomB and 0xffffffffffff to 12
+        ).joinToString("-") { (value, length) ->
+            value.asHexWithLength(length)
+        }
+    }
+
+    private fun Long.asHexWithLength(length: Int) = toString(16).padStart(length, '0')
+}
--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,21 +1,3 @@
-import kotlin.random.Random
-import kotlinx.datetime.Clock
-
-private val UNIX_TS_MS: Long = Clock.System.now().toEpochMilliseconds()
-private const val VERSION: Long = 7
-private const val VARIANT: Long = 0b10
-private val RAND_A: Long = Random.nextLong(0xfff) // 12 bits
-private val RAND_B: Long = Random.nextLong(0x3fffffffffffffff) // 62 bits
-
-private fun Long.asHexWithLength(length: Int) = toString(16).padStart(length, '0')
-
 fun main(args: Array<String>) {
-    val uuid = setOf(
-        (UNIX_TS_MS shr 16).asHexWithLength(8),
-        (UNIX_TS_MS and 0xffff).asHexWithLength(4),
-        ((VERSION shl 12) + RAND_A).asHexWithLength(4),
-        ((VARIANT shl 14) + (RAND_B shr 48)).asHexWithLength(4),
-        (RAND_B and 0xffffffffffff).asHexWithLength(12)
-    )
-    println(uuid.joinToString("-"))
+    println(Generate.now())
 }
