fd218e4c00243cd6a417ff92510776a1a8b6ed28
sergiocostamoreno <47816978+sergiocostamoreno@users.noreply.github.com>
2024-07-28 20:36:14 +0200

    Implementation following the RFC 9562 published by IETF

--- README.md
+++ README.md
@@ -8,11 +8,10 @@ A tiny project intended to play with Kotlin Native and UUIDs version 7.
 2. Generate the `kexe` file by running `./gradlew build` inside the folder of the repository.
 3. Start generating UUIDs v7 by running `./build/bin/native/releaseExecutable/uuid-cli.kexe` from your terminal inside the folder of the repository.
 
-Bear in mind, that the current implementation generates UUIDs using nanoseconds precision.
-
 Do you want to generate several UUIDv7 at the same time? Try running `seq 10 | xargs -I {} ./build/bin/native/releaseExecutable/uuid-cli.kexe` from your terminal.
 
 ## Future plans
-- Support milli and microseconds and choosing among them via the CLI.
 - Decode UUIDs.
 - Generate UUIDs based on a given timestamp.
+
+This implementation follows the [RFC 9562](https://datatracker.ietf.org/doc/rfc9562/).
--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,30 +1,21 @@
 import kotlin.random.Random
 import kotlinx.datetime.Clock
 
-private val NOW = Clock.System.now()
-private val epochSeconds: Long = NOW.epochSeconds
-private val nanos: Int = NOW.nanosecondsOfSecond
+private val UNIX_TS_MS: Long = Clock.System.now().toEpochMilliseconds()
 private const val VERSION: Long = 7
-private const val VARIANT: Long = 2
-private fun getSequence(): Int =
-    TODO("No bits reserved to avoid clashes between UUIDs generated on the same nanosecond with the same random bits")
+private const val VARIANT: Long = 0b10
+private val RAND_A: Long = Random.nextLong(0xfff) // 12 bits
+private val RAND_B: Long = Random.nextLong(0x3fffffffffffffff) // 62 bits
 
-//private fun getRandom(): Long = Random.nextLong(0x10000000000) // 40 bits if sequence bits were generated
-private val random: Long = Random.nextLong(0x1000000000000) // 48 bits
-
-private fun Int.getFirstChunk() = (((this shr 14) shr 12) and 0xfff).toLong()
-private fun Int.getSecondChunk() = ((this shr 14) and 0xfff).toLong()
-private fun Int.getThirdChunk() = (this and 0x3fff).toLong()
-
-private fun Long.toHex(length: Int) = toString(16).padStart(length, '0')
+private fun Long.asHexWithLength(length: Int) = toString(16).padStart(length, '0')
 
 fun main(args: Array<String>) {
     val uuid = setOf(
-        (epochSeconds shr 4).toHex(8),
-        ((epochSeconds and 0b1111 shl 12) + nanos.getFirstChunk()).toHex(4),
-        ((VERSION shl 12) + nanos.getSecondChunk()).toHex(4),
-        ((VARIANT shl 14) + nanos.getThirdChunk()).toHex(4),
-        random.toHex(12)
+        (UNIX_TS_MS shr 16).asHexWithLength(8),
+        (UNIX_TS_MS and 0xffff).asHexWithLength(4),
+        ((VERSION shl 12) + RAND_A).asHexWithLength(4),
+        ((VARIANT shl 14) + (RAND_B shr 48)).asHexWithLength(4),
+        (RAND_B and 0xffffffffffff).asHexWithLength(12)
     )
     println(uuid.joinToString("-"))
 }
