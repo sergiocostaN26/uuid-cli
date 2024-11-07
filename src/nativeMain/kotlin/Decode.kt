4fb48e09ba20f481816b91df0462657111b29989
sergiocostaN26 <vikingsouthman@proton.me>
2024-11-07 21:49:48 +0100

    Decode object and test finished

--- src/nativeMain/kotlin/Decode.kt
+++ src/nativeMain/kotlin/Decode.kt
@@ -1,3 +1,12 @@
+import kotlinx.datetime.Instant
+
 object Decode {
-    fun from(uuid: String): String = TODO()
+    fun from(uuid: String): String =
+        uuid.replace("-", "")
+            .take(12)
+            .let(::fromHexadecimal)
+            .let(Instant.Companion::fromEpochMilliseconds)
+            .toString()
+
+    private fun fromHexadecimal(hexadecimal: String): Long = hexadecimal.toLong(16)
 }
--- src/nativeTest/kotlin/DecodeTest.kt
+++ src/nativeTest/kotlin/DecodeTest.kt
@@ -6,12 +6,13 @@ class DecodeTest {
     fun `should decode from uuid with dashes`() {
         val result = Decode.from("019123b6-1be1-725f-82fd-7442c1f2ef78")
 
-        assertEquals("05-08-2024T18:03:42Z", result)
+        assertEquals("2024-08-05T18:03:42.945Z", result)
     }
+
     @Test
     fun `should decode from uuid without dashes`() {
         val result = Decode.from("019123b61be1725f82fd7442c1f2ef78")
 
-        assertEquals("05-08-2024T18:03:42Z", result)
+        assertEquals("2024-08-05T18:03:42.945Z", result)
     }
 }
